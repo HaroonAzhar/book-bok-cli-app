d3a50d35102356e59c7b39205288e4682bc793d6
MacBook Pro <macbookpro@Haroons-System.local>
2017-08-04 11:04:22 +0500

    break point 1

--- lib/book_box/qoute.rb
+++ lib/book_box/qoute.rb
@@ -3,16 +3,29 @@ class Qoute
 	attr_writer   :author_link
 
     def self.qoute_interface
-    	puts "---------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-    	puts "Qoutes of the day"
-    	puts "---------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-
-    	puts "Enter a keyword to search qoutes for"
-    	puts "Enter popular search for popular qoutes by likes"
-
-    	
+    	#qoute_of_the_day
+    	# puts "---------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+    	# puts "Qoutes of the day"
+    	# puts "---------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+        selection=nil
+        while selection!="exit"
+    		puts "Enter a keyword to search qoutes for"
+    		puts "Enter popular search for popular qoutes by likes"
+    		puts "Enter exit to quit searching for qoutes"
+    		selection=gets.strip.downcase
+    		case selection
+    			when "popular"
+    			when  "exit" 
+    		       break  	 
+    			else 
+    				## scrape keywords
+    		end
+    	end
     end
-	def self.create_by_scrape(link)
+	def self.scrape_popular
+		
+	end
+	def self.scrape_keywords
 		
 	end
 	
